ecb167bdf7c7d8fc5a1a0b6e00cb1b59648a795c
Isabele Vizoste <isavizoste@gmail.com>
2024-03-19 14:28:12 +0000

    aparentemente eu sei pensar sim

--- desafio3.js
+++ desafio3.js
@@ -1,6 +1,6 @@
 //De	Para	Fórmula
-//Celsius	Fahrenheit	tF = (tC * 9/5) + 32
-//Celsius	Kelvin	tK = tC + 273.15
+//Celsius	Fahrenheit	tF = (tC * 9/5) + 32 ~~
+//Celsius	Kelvin	tK = tC + 273.15 ~~
 //Fahrenheit	Celsius	tC = (tF-32) * 5/9
 //Fahrenheit	Kelvin	tK = (tF + 459.67) * 5/9
 //Kelvin	Celsius	tC = tK - 273.15
@@ -8,18 +8,35 @@
 
 
 const 
-    temperaturaAtual = window.prompt ('Digite o valor da temperatura:'),
-    escala = window.prompt ('Digite a escala da sua temperatura atual:'),
-    escalaConvertida = window.prompt ('Digite a nova escala do valor:'),
+    temperaturaAtual = parseFloat (window.prompt ('Digite o valor da temperatura:')),
+    escalaAtual = window.prompt ('Digite a escala da sua temperatura atual: (C, F ou K)').toUpperCase(),
+    escalaConvertida = window.prompt ('Digite a nova escala do valor: (C, F ou K)').toUpperCase(),
+    celsius = 'C',
+    fahrenheit = 'F',
+    kelvin = 'K'
 
-const
-    celsiusFahrenheit = (tF = (tC * 9/5) + 32)
 
-
-if ( valor de celsius) {
-    if 
-    console.log ( valor de farenheint)
-    else 
-    console.log ( valor de )
+if (escalaAtual == celsius) {
+    if ( escalaConvertida == fahrenheit) {
+        window.alert(`A temperatura de ${temperaturaAtual}ºC quando convertida é igual a ${(temperaturaAtual * 9/5)+32}ºF.`) ;
+     } else {
+        window.alert(`A temperatura de ${temperaturaAtual}ºC quando convertida é igual a ${temperaturaAtual + 273.15}ºK.`);
+     }
+}  if (escalaAtual == fahrenheit) {
+    if ( escalaConvertida == celsius) {
+        window.alert(`A temperatura de ${temperaturaAtual}ºF quando convertida é igual a ${(temperaturaAtual  - 32) * 5/9}ºC.`);
+     } else  {
+        window.alert(`A temperatura de ${temperaturaAtual}ºF quando convertida é igual a ${(temperaturaAtual  + 459.67) * 5/9}ºK .`);
+     }
+} if (escalaAtual == kelvin) {
+    if ( escalaConvertida == celsius) {
+        window.alert(`A temperatura de ${temperaturaAtual}ºK quando convertida é igual a ${temperaturaAtual - 273.15}ºC.`);
+     } else  {
+        window.alert(`A temperatura de ${temperaturaAtual}ºK quando convertida é igual a ${temperaturaAtual  * 9/5 - 459.67}ºF.`);
+     }
+}  if (escalaAtual, escalaConvertida, temperaturaAtual != fahrenheit, kelvin, celsius) {
+   window.alert(`O dados de entrada não são validos`)
 }
-    
+
+
+
--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,6 +1,6 @@
-const a = false, b = false;
+const a = true, b = false, c = true, d = false;
 
-if (a || b) {
+if (a || b && c || d) {
     console.log('VERDADEIRO');
 } else {
     console.log('FALSO');

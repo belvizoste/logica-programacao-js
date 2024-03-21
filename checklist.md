73f52a3ec508e1d434f2f058dc798c350f35a80d
belvizoste <isavizoste@gmail.com>
2024-03-21 10:47:22 -0300

    Criando botões

---
+++ checklist.md
@@ -0,0 +1,30 @@
+# Setup inicial de um repositório do Github no VS Code local: 
+
+1. Limpar credenciais do sistema (windows);
+2. Limpar os dados do navegador; 
+3. Abrir o repositório no GitHub;
+4. Copiar a URL do repositório (HTTPS);
+5. Abrir o VS Code; 
+6. Se houver algum projeto aberto no VS Code, fechar a pasta (CTRL + K F);
+7. Se houver algum usuário logado no VS Code, clique em "sair";
+8. Verificar se existem variáveis `user.name` e `user.email` globais:
+~~~bash
+git config --global user.name 
+git config --global user.email
+~~~
+9. Se as variáveis retornarem algum valor, redefina ambas: 
+~~~bash
+git config --global --unset user.name 
+git config --global --unset user.email
+~~~
+10. Faça a clonagem do repositório no computador local: 
+~~~bash
+git clone https://github.com/usuario/nomedorepositório.git
+~~~
+11. Abra o projeto clonado no VS Code:
+~~~bash
+code -r nomedorepositoriolocal 
+~~~
+12. Faça login no VS Code local para sincronizar suas extensões e temas;
+13. Ative o salvamento automático _(Auto Save)_
+
--- desafio2.js
+++ desafio2.js
@@ -1,23 +1,33 @@
-const 
-    peso = window.prompt(`Digite seu peso:`),
-    altura = window.prompt(`Digite sua altura:`),
-    imc = peso / altura**2; 
-
-if (imc < 18.5) {
-    window.alert(`IMC: ${imc.toFixed(1)} --> Abaixo do peso`);
+function calculaIMC() {
     
-} else if (imc < 25.0) {
-    window.alert(`IMC: ${imc.toFixed(1)} --> Peso normal`);
+    const 
+        peso = window.prompt(`Digite seu peso:`),
+        altura = window.prompt(`Digite sua altura:`),
+        imc = peso / altura**2; 
+    
+    if (imc < 18.5) {
+        window.alert(`IMC: ${imc.toFixed(1)} --> Abaixo do peso`);
+        
+    } else if (imc < 25.0) {
+        window.alert(`IMC: ${imc.toFixed(1)} --> Peso normal`);
+    
+    } else if (imc < 30.0) {
+        window.alert(`IMC: ${imc.toFixed(1)} --> Sobrepeso`);
+    
+    } else if (imc < 35.0) {
+        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau I`);
+    
+    } else if (imc < 40.0) {
+        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau II`); 
+    
+    } else {
+        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau III`);
+    }
 
-} else if (imc < 30.0) {
-    window.alert(`IMC: ${imc.toFixed(1)} --> Sobrepeso`);
 
-} else if (imc < 35.0) {
-    window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau I`);
-
-} else if (imc < 40.0) {
-    window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau II`); 
-
-} else {
-    window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau III`);
 }
+
+
+
+
+

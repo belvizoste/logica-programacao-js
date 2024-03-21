b1f54161c927f4d7fe9ffe0a8354fa64c6f4efa2
belvizoste <isavizoste@gmail.com>
2024-03-21 11:49:15 -0300

    criando botões em páginas html

--- desafio2.js
+++ desafio2.js
@@ -1,29 +1,29 @@
-function calculaIMC() {
+function calculaIMC(peso, altura) {
     
     const 
-        peso = window.prompt(`Digite seu peso:`),
-        altura = window.prompt(`Digite sua altura:`),
         imc = peso / altura**2; 
+        let mensagem; 
     
     if (imc < 18.5) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Abaixo do peso`);
+        mensagem = `IMC: ${imc.toFixed(1)} --> Abaixo do peso`;
         
     } else if (imc < 25.0) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Peso normal`);
+         mensagem = `IMC: ${imc.toFixed(1)} --> Peso normal`;
     
     } else if (imc < 30.0) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Sobrepeso`);
+        mensagem = `IMC: ${imc.toFixed(1)} --> Sobrepeso`;
     
     } else if (imc < 35.0) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau I`);
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau I`;
     
     } else if (imc < 40.0) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau II`); 
+         mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau II`; 
     
     } else {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau III`);
+       mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau III`;
     }
 
+    return mensagem; 
 
 }
 

6bff0f22965878059c0c7a6fcf929dded8ed8585
belvizoste <isavizoste@gmail.com>
2024-05-07 11:50:10 -0300

    style: responsividade da tela

--- js/navbar.js
+++ js/navbar.js
@@ -34,6 +34,51 @@ function criaUlNav(ancoras){
     return ul;
 };
 
+// <nav> do menu
+const nav = document.createElement('nav');
+nav.classList.add('menu-navegacao');
+nav.appendChild(criaUlNav(ancoras));
+
+// Aqui segue de acordo com a solução do professor...
+
+// <span>Desafios & Exercícios</span>
+const spanLogo = document.createElement('span');
+spanLogo.innerHTML = 'Exercícios';
+
+// <div class="logo">
+const divLogo = document.createElement('div');
+divLogo.classList.add('logo');
+divLogo.appendChild(spanLogo);
+
+// <div class="logo-menu">
+const divLogoMenu = document.createElement('div');
+divLogoMenu.classList.add('logo-menu');
+divLogoMenu.append(divLogo, nav);
+
+// <header>
+const header = document.createElement('header');
+header.appendChild(divLogoMenu);
+
+// document.body.insertBefore(nav, document.body.firstChild);
+document.body.insertAdjacentElement('afterbegin', header);
+
+
+// ** Estilos do Navbar **
+const linkNavbar = document.createElement('link');
+linkNavbar.setAttribute('rel', 'stylesheet');
+linkNavbar.setAttribute('href', '/css/navbar.css');
+
+document.querySelector('[href="/css/responsividade.css"]').insertAdjacentElement('beforebegin', linkNavbar);
+
+
+
+
+
+
+
+
+
+
 // // comentário de criação de ul e li 
 //<a> do Home
 // const aHome = criaAncora ('/index.html'/ 'Home');
@@ -62,38 +107,3 @@ function criaUlNav(ancoras){
 // ulNav.append(liHome, liExercicio1, liExercicio3);
 
 // <nav> do menu
-
-const nav = document.createElement('nav');
-nav.classList.add('menu-navegacao');
-nav.appendChild(ulNav);
-
-// Aqui segue de acordo com a solução do professor...
-
-// <span>Desafios & Exercícios</span>
-const spanLogo = document.createElement('span');
-spanLogo.innerHTML = 'Desafios & Exercícios';
-
-// <div class="logo">
-const divLogo = document.createElement('div');
-divLogo.classList.add('logo'); 
-divLogo.appendChild(spanLogo);
-
-// <div class="logo-menu">
-const divLogoMenu = document.createElement('div');
-divLogoMenu.classList.add('logo-menu');
-divLogoMenu.append(divLogo, nav);
-
-const header = document.createElement('header');
-header.appendChild(divLogoMenu);
-
-// document.body.insertBefore(nav, document.body.firstChild);
-document.body.insertAdjacentElement('afterbegin', header);
-
-
-// Estilos do Navbar
-const linkNavbar = document.createElement('link');
-linkNavbar.setAttribute('rel', 'stylesheet');
-linkNavbar.setAttribute('href', '/css/navbar.css');
-
-document.head.appendChild(linkNavbar);
-

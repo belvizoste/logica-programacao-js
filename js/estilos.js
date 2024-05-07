f32ce47328b6e1e90e3b6a243c6b494e297efa86
belvizoste <isavizoste@gmail.com>
2024-05-07 08:48:04 -0300

    refact: automatizando a criação de links do css

---
+++ js/estilos.js
@@ -0,0 +1,13 @@
+// pra automatizar os links das páginas de css nas páginas html 
+const links = [
+    {rel: "stylesheet", href:"/css/reset.css"},
+    {rel: "stylesheet", href:"/css/style.css"},
+    {rel: "stylesheet", href:"/css/responsividade.css"},
+];
+
+links.forEach(link => {
+    const l =document.createElement('link');
+    l.rel = link.rel;
+    l.href = link.href; 
+    document.head.appendChild(l);
+});
